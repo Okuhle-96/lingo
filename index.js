0051c0a09d104b9bd9d3e5828555dc5aac813788
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2021-12-03 10:54:57 +0200

    added expressjs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-function names(){
-    let thename = document.querySelector(".name").value;
-    console.log(thename);
-    localStorage.setItem('names', JSON.stringify(thename));
-    
-}
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const PORT =  process.env.PORT || 3030;
+
+// start  the server and start listening for HTTP request on the PORT number specified...
+app.listen(PORT, function() {
+	console.log(`App started on port ${PORT}`)
+});
---
+++ public/index.js
@@ -0,0 +1,6 @@
+function names(){
+    let thename = document.querySelector(".name").value;
+    console.log(thename);
+    localStorage.setItem('names', JSON.stringify(thename));
+    
+}
